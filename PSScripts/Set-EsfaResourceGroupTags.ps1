0ad7187415cf97378d4c9266ede7027f6ad995e9
Olusola Adio <Olusola.Adio@SweatyBetty.com>
2022-05-01 21:26:27 +0100

    first commit

--- PSScripts/Set-EsfaResourceGroupTags.ps1
+++ PSScripts/Set-EsfaResourceGroupTags.ps1
@@ -37,7 +37,7 @@ param(
     [ValidateSet("Sweaty Betty")]
     [string]$ParentBusiness,
     [Parameter(Mandatory=$true)]
-    [ValidateSet("Business Central", "Business Central (PP)", "Mulesoft")]
+    [ValidateSet("Shared AKS", "Business Central", "Business Central (PP)", "Mulesoft")]
     [string]$ServiceOffering
 )
 
