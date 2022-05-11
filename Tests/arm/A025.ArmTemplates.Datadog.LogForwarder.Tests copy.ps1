9f72086f74f6d45eb3b4fb2fff716fd5f2d5e7fd
Olusola Adio <Olusola.Adio@SweatyBetty.com>
2022-05-11 08:54:25 +0100

    test

---
+++ Tests/arm/A025.ArmTemplates.Datadog.LogForwarder.Tests copy.ps1
@@ -0,0 +1,53 @@
+
+Describe "Datadog Tests" -Tag "Acceptance" {
+
+  BeforeAll {
+    # common variables
+    $ResourceGroupName = "sb-test-template-rg"
+    $TemplateFile = "$PSScriptRoot\..\..\ArmTemplates\Datadog\event-hub-log-forwarder.json"
+  }
+
+  Context "When an Event Grid Topic is deployed with a name and sku" {
+
+    BeforeAll {
+
+      $prefix = "sb-test-template"
+      $DatadogSite = "datadoghq.eu"
+
+      $EventhubNamespace = "$($prefix)-eh-ns"
+      $FunctionAppName ="$($prefix)-fa"
+      $EventhubName = "$($prefix)-eh"
+      $FunctionName = "$($prefix)-fn"
+      $functionAppNameInsights = "$($prefix)-ai"
+      $ApiKey = "kjhjkhkj"
+
+      $code = (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/olusola-adio-sweaty/sb-devops/master/ArmTemplates/Datadog/function-code.js")
+
+
+      $endpointSuffix = "core.windows.net"
+
+      $TemplateParameters = @{
+          functionCode = $code
+          apiKey = $ApiKey
+          eventHubName = $EventhubName
+          functionName = $FunctionName
+          datadogSite = $DatadogSite
+          endpointSuffix = $endpointSuffix
+          eventhubNamespace = $EventhubNamespace
+          functionAppNameInsights = $functionAppNameInsights
+          functionAppName = $FunctionAppName
+      }
+  
+      $TestTemplateParams = @{
+        ResourceGroupName       = $ResourceGroupName
+        TemplateFile            = $TemplateFile
+        TemplateParameterObject = $TemplateParameters
+      }
+ 
+    }
+    It "Should be deployed successfully" {
+      $output = Test-AzureRmResourceGroupDeployment @TestTemplateParams
+      $output | Should -Be $null
+    }
+  }
+}
